f5a2c781fc9e042c6e07ada0c4e4faaaec4b1b4c
jronatay <jronatay25@gmail.com>
2015-03-03 18:02:37 +0800

    update

--- public_html/js/main.js
+++ public_html/js/main.js
@@ -202,42 +202,67 @@
  };
  });
  
- */
+ 
+ 
+ 
+ var app = angular.module('app', []);
+ 
+ app.directive('country', function () {
+ return{
+ restrict: 'E',
+ controller: function () {
+ this.makeAnnouncement = function (message) {
+ console.log("Country says " + message);
+ };
+ 
+ }
+ 
+ };
+ });
+ 
+ app.directive('state', function () {
+ return{
+ restrict: 'E',
+ controller: function () {
+ this.makeLaw = function (law) {
+ console.log("Law " + law);
+ };
+ }
+ };
+ });
+ 
+ app.directive('city', function () {
+ return{
+ restrict: 'E',
+ require: ["^country","^state"],
+ link: function (scope, element, attrs, ctrls) {
+ ctrls[0].makeAnnouncement("This city rocks!");
+ ctrls[1].makeLaw("Jump Higher!");
+ }
+ };
+ });
+ 
+ 
 
+var app = angular.module('choreApp', []);
 
-var app = angular.module('app', []);
+function choreCtrl($scope) {
+    $scope.logChore = function (chore) {
+        alert(chore + " is done!");
+    };
+}
 
-app.directive('country', function () {
+app.controller('choreCtrl', choreCtrl);
+app.directive('kid', function () {
     return{
         restrict: 'E',
-        controller: function () {
-            this.makeAnnouncement = function (message) {
-                console.log("Country says " + message);
-            };
-
-        }
-
+        scope: {
+            done: "&"
+        },
+        template: '<input type="text" ng-model="chore">{{chore}}' +
+                '{{chore}}' +
+                '<div class="button" ng-click="done({chore: chore})">I\'m done! </div>'
     };
 });
 
-app.directive('state', function () {
-    return{
-        restrict: 'E',
-        controller: function () {
-            this.makeLaw = function (law) {
-                console.log("Law " + law);
-            };
-        }
-    };
-});
-
-app.directive('city', function () {
-    return{
-        restrict: 'E',
-        require: ["^country","^state"],
-        link: function (scope, element, attrs, ctrls) {
-            ctrls[0].makeAnnouncement("This city rocks!");
-            ctrls[1].makeLaw("Jump Higher!");
-        }
-    };
-});
+*/

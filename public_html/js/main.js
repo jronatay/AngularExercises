9532e0ad5f11f6fca15e064765146d765baa6ae8
jronatay <jronatay25@gmail.com>
2015-03-03 18:23:01 +0800

    update

--- public_html/js/main.js
+++ public_html/js/main.js
@@ -266,3 +266,17 @@ app.directive('kid', function () {
 });
 
 */
+
+var app=angular.module('drinkApp',[]);
+app.controller('appCtrl',function($scope){
+    $scope.ctrlFlavor='blackberry';
+});
+app.directive('drink',function(){
+    return{
+      scope:{
+          flavor:"="
+      },
+      template:'<div>{{flavor}}</div>'
+      
+    };
+});

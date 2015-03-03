75cfa66f689bc6390575c969383edeccc125c3c7
jronatay <jronatay25@gmail.com>
2015-03-03 14:43:31 +0800

    first commit

--- public_html/js/main.js
+++ public_html/js/main.js
@@ -21,3 +21,12 @@ function secondCtrl($scope, Data) {
     
 
 }
+
+var app=angular.module('superhero',[])
+
+app.directive('superman',function(){
+    return{
+      restrict:"E",
+      template:"<div>Here I am to save the day</div>"
+    };
+});
